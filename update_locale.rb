1dc92e0efb9eb6c9506281f265ae69502adcdde8
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2020-08-05 11:56:16 +0200

    disable compiling of .mo files

--- update_locale.rb
+++ update_locale.rb
@@ -97,8 +97,10 @@ Dir.glob("locales/**/*.po").each do |file|
   `msgmerge --update --no-wrap #{file} locales/branding.pot`
 end
 
-Dir.glob("./locales/*/*/") do |locale_dir|
-  po_file = "#{locale_dir}branding.po"
-  mo_file = "#{locale_dir}branding.mo"
-  `msgfmt #{po_file} -o #{mo_file}`
-end
+# .mo file compilation is now handled in
+# https://translate.quickpay.net/projects/quickpay/standard-branding
+# Dir.glob("./locales/*/*/") do |locale_dir|
+#   po_file = "#{locale_dir}branding.po"
+#   mo_file = "#{locale_dir}branding.mo"
+#   `msgfmt #{po_file} -o #{mo_file}`
+# end
